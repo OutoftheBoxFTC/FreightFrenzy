ea6e19c8dc713f0cf1f9aa814f514cc0fc33f90e
Eeshwar-Krishnan <32040858+Eeshwar-Krishnan@users.noreply.github.com>
2022-02-03 16:41:07 -0500

    tweaked teleop retraction protocol

--- TeamCode/src/main/java/Hardware/HardwareSystems/FFSystems/Actions/MoveTurretAction.java
+++ TeamCode/src/main/java/Hardware/HardwareSystems/FFSystems/Actions/MoveTurretAction.java
@@ -2,7 +2,6 @@ package Hardware.HardwareSystems.FFSystems.Actions;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.qualcomm.robotcore.util.RobotLog;
 
 import Hardware.HardwareSystems.FFSystems.FFConstants;
 import Hardware.HardwareSystems.FFSystems.TurretSystem;
@@ -53,6 +52,10 @@ public class MoveTurretAction implements Action {
         double power = pid.getCorrection(MathUtils.getRotDist(system.getTurretPosition(), targetAngle).degrees());
         system.setTurretMotorPower(MathUtils.signedMax(power, FFConstants.Turret.TURRET_KSTATIC));
 
+
+        FtcDashboard.getInstance().getTelemetry().addData("Error", MathUtils.getRotDist(system.getTurretPosition(), targetAngle).degrees());
+        FtcDashboard.getInstance().getTelemetry().addData("Corr", pid.getCorrection(MathUtils.getRotDist(system.getTurretPosition(), targetAngle).degrees()));
+        FtcDashboard.getInstance().getTelemetry().addData("Target", targetAngle.degrees());
     }
 
     @Override
